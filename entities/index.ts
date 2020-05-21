5afebe78ea83baac266988c42fed502225b691fc
Chasky Hoffmann <Chasky.h@pepperi.com>
2020-05-21 12:59:19 +0300

    fixed some publish issues

--- entities/index.ts
+++ entities/index.ts
@@ -1,2 +1,6 @@
 export * from './addons'
 export * from './user-defined-tables'
+
+// need something here that can be transpiled to js
+// all the other entities are interfaces
+export class Entities {}
