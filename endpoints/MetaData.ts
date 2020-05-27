20bd1f395fd4b2181a32b8037b1bea8a6a1c97ad
Shir Ohana <shir.o@pepperi.com>
2020-05-27 14:36:24 +0300

    more changes

--- endpoints/MetaData.ts
+++ endpoints/MetaData.ts
@@ -3,11 +3,18 @@ import { PapiClient } from "../papi-client";
 
 
 export class DistributorFlagsEndpoint 
-{
-    constructor(private service: PapiClient) { }
+{ 
+    private options = {
+        name:''
+    }
+    constructor (private service: PapiClient) {}
 
-    async name(flagName:string): Promise<object> {
-        return await this.service.get(`/meta_data/flags/${flagName}`);
+    name(flagName:string) {
+        this.options.name = flagName;
+        return this;
+    }
 
+    async get(): Promise<object> {
+        return await this.service.get(`/meta_data/flags/${this.options.name}`); 
     }
 }
