c2e5c3aed9b3cc68b6c9d1d3591e9e1e947046e0
roiaharon_pepperi <roi.a@pepperi.com>
2020-06-15 11:04:51 +0300

    implementing iter function on endpoint

--- endpoint.ts
+++ endpoint.ts
@@ -30,6 +30,48 @@ export default class Endpoint<T> {
     async upsert(object: T): Promise<T> {
         return this.service.post(this.endpoint, object);
     }
+       
+    iter(options: FindOptions = {}) {
+        let self = this;
+        return {
+            [Symbol.asyncIterator]() : AsyncIterator<T> {
+                let newOptions: FindOptions & {include_count?:boolean} = options;
+                newOptions.include_count = true;
+                let currentPage = 1;
+                let obj:{ items: T[], numOfPages:number } = { items: [], numOfPages: 1 };
+                return {
+                    next: async () => {
+                        if (obj.items.length == 0 ) {
+                            if (currentPage == 1) {
+                                newOptions.page = currentPage++;
+                                obj = await self.getFirstPage(newOptions);
+                            }
+                            else if(currentPage <= obj.numOfPages) {
+                                newOptions.page = currentPage++;
+                                obj.items = await self.find(newOptions);    
+                            }
+                        }
+                        let retItem = obj.items.shift();
+                        if (retItem) {
+                            return { value: retItem, done: false };
+                        }
+                        return { value: {}, done: true };
+                    }
+                }
+            }
+        }
+    }
+
+    async getFirstPage(options:any): Promise<{items:T[], numOfPages:number}> {
+        let url = this.endpoint;
+        let query = Endpoint.encodeQueryParams(options);
+        let items: T[] = [];
+        url = query ? url + "?" + query : url;
+        let res = await this.service.apiCall('GET',url);
+        let numOfPages = Number(res.headers.get('X-Pepperi-Total-Pages'));
+        items = await res.json();
+        return {items, numOfPages};
+    }
 
     static encodeQueryParams(params: any) {
         const ret: string[] = [];
@@ -40,4 +82,5 @@ export default class Endpoint<T> {
 
         return ret.join('&');
     }
+
 }
