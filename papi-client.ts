d357baa108b36793639c6e2e4b0b5fa972b7cc1f
PEPPERI\tomer.p <tomer.p@pepperi.com>
2020-06-17 11:37:27 +0300

    - Added delete function.

--- papi-client.ts
+++ papi-client.ts
@@ -1,24 +1,21 @@
+import Endpoint from './endpoint';
+import {AddonEndpoint, CodeJobsEndpoint, DistributorFlagsEndpoint} from './endpoints';
+import {UserDefinedTableMetaData, UserDefinedTableRow, Account, GeneralActivity, Transaction} from './entities';
+import {performance} from 'perf_hooks';
+import fetch from 'node-fetch';
+import {User} from './entities/user';
 
-import Endpoint from "./endpoint";
-import { AddonEndpoint, CodeJobsEndpoint,DistributorFlagsEndpoint } from "./endpoints";
-import { UserDefinedTableMetaData, UserDefinedTableRow, Account, GeneralActivity, Transaction } from "./entities" ;
-import { performance } from 'perf_hooks';
-import fetch from 'node-fetch'
-import { User } from "./entities/user";
-
-type HttpMethod =  'POST' | 'GET' | 'PUT' | 'DELETE';
+type HttpMethod = 'POST' | 'GET' | 'PUT' | 'DELETE';
 
 interface PapiClientOptions {
-    token: string, 
-    baseURL: string
-};
+    token: string;
+    baseURL: string;
+}
 
 export class PapiClient {
-    
     metaData = {
         userDefinedTables: new Endpoint<UserDefinedTableMetaData>(this, '/meta_data/user_defined_tables'),
-        flags : new DistributorFlagsEndpoint(this)
-
+        flags: new DistributorFlagsEndpoint(this),
     };
 
     userDefinedTables = new Endpoint<UserDefinedTableRow>(this, '/user_defined_tables');
@@ -29,32 +26,30 @@ export class PapiClient {
     allActivities = new Endpoint<GeneralActivity | Transaction>(this, '/all_activities');
     accounts = new Endpoint<Account>(this, '/accounts');
     users = new Endpoint<User>(this, '/users');
-  
-    
-    constructor(
-        private options: PapiClientOptions
-        ) {
-        
-    }
+
+    constructor(private options: PapiClientOptions) {}
 
     async get(url: string): Promise<any> {
-        return this.apiCall('GET', url).then(res => res.json());
+        return this.apiCall('GET', url).then((res) => res.json());
     }
 
     async post(url: string, body: any = undefined): Promise<any> {
-        return this.apiCall('POST', url, body).then(res => res.json());
+        return this.apiCall('POST', url, body).then((res) => res.json());
+    }
+
+    async delete(url: string): Promise<any> {
+        return this.apiCall('DELETE', url);
     }
 
     async apiCall(method: HttpMethod, url: string, body: any = undefined) {
-        
         const fullURL = this.options.baseURL + url;
         const options: any = {
             method: method,
             headers: {
-                authorization: 'Bearer ' + this.options.token
-            }
+                authorization: 'Bearer ' + this.options.token,
+            },
         };
-        
+
         if (body) {
             options.body = JSON.stringify(body);
         }
