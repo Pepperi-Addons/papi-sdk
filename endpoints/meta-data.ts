ad49919d2e38988a1b9aeffe056331f644069c80
Hadar Levi <hadar.l@pepperi.com>
2020-09-24 09:13:42 +0300

    fix

--- endpoints/meta-data.ts
+++ endpoints/meta-data.ts
@@ -58,7 +58,7 @@ export class Fields {
     constructor(private service: PapiClient, private type: string, private subtypeid?: string) {}
 
     async get(apiName: string): Promise<ApiFieldObject>;
-    async get(params: { include_owned: boolean }): Promise<ApiFieldObject[]>;
+    async get(params?: { include_owned: boolean }): Promise<ApiFieldObject[]>;
     async get(
         p: string | { include_owned: boolean } = { include_owned: true },
     ): Promise<ApiFieldObject | ApiFieldObject[]> {
