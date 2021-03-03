92963bef3490b27ba73e7e59aa4e1f5c6339824a
Hadar Levi <hadar.l@pepperi.com>
2021-03-03 11:51:22 +0200

    added field bank entity

---
+++ entities/field-bank.ts
@@ -0,0 +1,14 @@
+import { DataViewFieldType } from "./data-view";
+
+export interface FieldBank {
+  UUID?: string;
+  Hidden?: boolean;
+  CreationDateTime?: string;
+  ModificationDateTime?: string;
+  Title: string;
+  FieldType?: DataViewFieldType;
+  Group: string;
+  FieldID?: string;
+  FieldPrefix: string;
+  FieldParams?: any;
+}
