0bb5256ce270cc393c1635b1834a53c71830c69e
Chasky Hoffmann <Chasky.h@pepperi.com>
2021-03-03 13:22:50 +0200

    fix lint

--- entities/field-bank.ts
+++ entities/field-bank.ts
@@ -1,14 +1,14 @@
-import { DataViewFieldType } from "./data-view";
+import { DataViewFieldType } from './data-view';
 
 export interface FieldBank {
-  UUID?: string;
-  Hidden?: boolean;
-  CreationDateTime?: string;
-  ModificationDateTime?: string;
-  Title: string;
-  FieldType?: DataViewFieldType;
-  Group: string;
-  FieldID?: string;
-  FieldPrefix: string;
-  FieldParams?: any;
+    UUID?: string;
+    Hidden?: boolean;
+    CreationDateTime?: string;
+    ModificationDateTime?: string;
+    Title: string;
+    FieldType?: DataViewFieldType;
+    Group: string;
+    FieldID?: string;
+    FieldPrefix: string;
+    FieldParams?: any;
 }
