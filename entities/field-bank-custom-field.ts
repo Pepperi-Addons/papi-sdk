1af9eab062d61b041d4ca5df84dbaf7adc47c51d
Hadar Levi <hadar.l@pepperi.com>
2021-03-15 16:31:56 +0200

    delete old file

--- entities/field-bank-custom-field.ts
+++ entities/field-bank-custom-field.ts
@@ -9,3 +9,36 @@ export interface FieldBankCustomField extends AddonData {
     FieldPrefix: string;
     FieldParams?: any;
 }
+
+type AddonFieldType = 'Component' | 'Navigation' | 'BackgroundJob';
+type ComponentAddonSubType = 'NG11';
+type NavigationAddonSubType = 'Settings';
+type BackgroundJobAddonSubType = 'None' | 'DownloadURL';
+type AddonFieldSubType = ComponentAddonSubType | NavigationAddonSubType | BackgroundJobAddonSubType;
+interface AddonFieldBase {
+    Type: AddonFieldType;
+    SubType: AddonFieldSubType;
+    AddonUUID: string;
+    RelativeURL: string;
+    VisibleEndpoint?: string;
+    MultiSelection?: boolean;
+    Confirmation?: boolean;
+}
+export interface ComponentAddonField extends AddonFieldBase {
+    Type: 'Component';
+    SubType: ComponentAddonSubType;
+    ComponentName: string;
+    ModuleName: string;
+}
+
+export interface NavigationAddonField extends AddonFieldBase {
+    Type: 'Navigation';
+    SubType: NavigationAddonSubType;
+}
+
+export interface BackgroundJobAddonField extends AddonFieldBase {
+    Type: 'BackgroundJob';
+    SubType: BackgroundJobAddonSubType;
+}
+
+export type AddonField = ComponentAddonField | NavigationAddonField | BackgroundJobAddonField;
