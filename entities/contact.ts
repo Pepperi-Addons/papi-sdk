ca9f07b217ad59f7192427aebaae99183ebeba2a
amirf123 <81569287+amirf123@users.noreply.github.com>
2021-04-26 16:11:00 +0300

    Contact endpoint (#74)
    * Added entity 'Contact'
    
    * 1.23.0

---
+++ entities/contact.ts
@@ -0,0 +1,17 @@
+export interface Contact {
+    CreationDateTime?: string;
+    Email?: string;
+    Email2?: string;
+    ExternalID: string;
+    FirstName?: string;
+    Hidden?: boolean;
+    IsBuyer?: boolean;
+    LastName?: string;
+    Mobile?: string;
+    ModificationDateTime?: string;
+    Phone?: string;
+    Role?: string;
+    Status?: number;
+    TypeDefinitionID?: number;
+    UUID?: string;
+}
