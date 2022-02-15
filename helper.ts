eca68ce0724021ab1737f8560ae422e75da576e0
Nofar Elarat <nofar.e@pepperi.com>
2022-02-15 10:46:22 +0200

    fix code review comments

--- helper.ts
+++ helper.ts
@@ -1,10 +1,8 @@
 export class Helper {
-    ParseHeaders(requestHeaders: { [key: string]: string }) {
+    normalizeHeaders(requestHeaders: { [key: string]: string }) {
         const headers: { [key: string]: string } = {};
-        if (requestHeaders != null) {
-            for (const key in requestHeaders) {
-                headers[key.toLowerCase()] = requestHeaders[key];
-            }
+        for (const key in requestHeaders) {
+            headers[key.toLowerCase()] = requestHeaders[key];
         }
         return headers;
     }
