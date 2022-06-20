76b9d9d99de9f83c79177499846c754d386fa1b7
OdedAnswer <89458041+OdedAnswer@users.noreply.github.com>
2022-06-20 19:04:51 +0300

    added SDK for DIMX import and files import/export (#119)

--- endpoints/addons.ts
+++ endpoints/addons.ts
@@ -8,7 +8,16 @@ import {
     AddonDataScheme,
     Relation,
     AddonFile,
+    DIMXObject,
 } from '../entities';
+import {
+    DataImportInput,
+    FileExportInput,
+    FileImportInput,
+    MappingInput,
+    RecursiveExportInput,
+    RecursiveImportInput,
+} from '../entities/dimx_inputs';
 import { PapiClient } from '../papi-client';
 
 class InstalledAddonEnpoint {
@@ -167,6 +176,98 @@ export class AddonEndpoint extends Endpoint<Addon> {
         },
 
         relations: new Endpoint<Relation>(this.service, '/addons/data/relations'),
+        import: {
+            uuid: (addonUUID: string) => {
+                return {
+                    table: (tableName: string) => {
+                        return {
+                            upsert: async (body: DataImportInput): Promise<DIMXObject[]> => {
+                                return await this.service.post(`/addons/data/import/${addonUUID}/${tableName}`, body);
+                            },
+                        };
+                    },
+                };
+            },
+            file: {
+                uuid: (addonUUID: string) => {
+                    return {
+                        table: (tableName: string) => {
+                            return {
+                                upsert: async (body: FileImportInput): Promise<AddonAPIAsyncResult> => {
+                                    return await this.service.post(
+                                        `/addons/data/import/file/${addonUUID}/${tableName}`,
+                                        body,
+                                    );
+                                },
+                            };
+                        },
+                    };
+                },
+                recursive: {
+                    uuid: (addonUUID: string) => {
+                        return {
+                            table: (tableName: string) => {
+                                return {
+                                    upsert: async (body: RecursiveImportInput): Promise<AddonAPIAsyncResult> => {
+                                        return await this.service.post(
+                                            `/addons/data/import/file/recursive/${addonUUID}/${tableName}`,
+                                            body,
+                                        );
+                                    },
+                                };
+                            },
+                        };
+                    },
+                },
+            },
+        },
+        export: {
+            file: {
+                uuid: (addonUUID: string) => {
+                    return {
+                        table: (tableName: string) => {
+                            return {
+                                get: async (body: FileExportInput): Promise<AddonAPIAsyncResult> => {
+                                    return await this.service.post(
+                                        `/addons/data/export/file/${addonUUID}/${tableName}`,
+                                        body,
+                                    );
+                                },
+                            };
+                        },
+                    };
+                },
+                recursive: {
+                    uuid: (addonUUID: string) => {
+                        return {
+                            table: (tableName: string) => {
+                                return {
+                                    get: async (body: RecursiveExportInput): Promise<AddonAPIAsyncResult> => {
+                                        return await this.service.post(
+                                            `/addons/data/export/file/recursive/${addonUUID}/${tableName}`,
+                                            body,
+                                        );
+                                    },
+                                };
+                            },
+                        };
+                    },
+                },
+            },
+        },
+        mapping: {
+            uuid: (addonUUID: string) => {
+                return {
+                    table: (tableName: string) => {
+                        return {
+                            upsert: async (body: MappingInput): Promise<AddonAPIAsyncResult> => {
+                                return await this.service.post(`/addons/data/mapping/${addonUUID}/${tableName}`, body);
+                            },
+                        };
+                    },
+                };
+            },
+        },
     };
 
     pfs = {
