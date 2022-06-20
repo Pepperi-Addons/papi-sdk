5ae198e6df7f8266c64c0065bcbb29aa8465487e
Oded Answer <oded.t@pepperi.com>
2022-06-20 08:43:04 +0300

    comments and order

--- entities/dimx_inputs.ts
+++ entities/dimx_inputs.ts
@@ -1,3 +1,4 @@
+// In revursive import and recursive create mapping, we send the data of inner resources. The resources are described in the following format:
 interface ResourceObject {
     URI: string;
     OverwriteObject: boolean;
@@ -7,21 +8,37 @@ interface ResourceObject {
     Version: string;
 }
 
+// Imports:
+
+// Basic import parameters. all imports have these optional parameters and they all extend this interface.
 interface BaseImportInput {
     OverwriteObject?: boolean;
     OverwriteTable?: boolean;
     Version?: string;
 }
 
+// input to the Data Import function.
+export interface DataImportInput extends BaseImportInput {
+    Objects: any[];
+}
+
+// Basic file import. All file imports must include this parameter.
 interface BaseFileImportInput extends BaseImportInput {
     URI: string;
 }
 
+// input to the File Import function.
+export interface FileImportInput extends BaseFileImportInput {
+    Delimiter?: string;
+}
+
+// input to the function that creates the recursive mapping object, which is an optional parameter for the recursive import function.
 export interface MappingInput {
     URI: string;
     Resources: ResourceObject[];
 }
 
+// input to the File Import function.
 export interface RecursiveImportInput extends BaseFileImportInput {
     Resources: ResourceObject[];
     Mapping: {
@@ -34,6 +51,9 @@ export interface RecursiveImportInput extends BaseFileImportInput {
     };
 }
 
+// Exports:
+
+// input to the Data Export function.
 export interface RecursiveExportInput {
     IncludeDeleted?: boolean;
     Where?: string;
@@ -41,15 +61,8 @@ export interface RecursiveExportInput {
     ExcludedKeys?: string[];
 }
 
+// input to the Data Export function. This function accepts all parameters of the Data Export function and extends it with the following parameters.
 export interface FileExportInput extends RecursiveExportInput {
     Format?: 'csv' | 'json';
     Delimiter?: string;
-}
-
-export interface FileImportInput extends BaseFileImportInput {
-    Delimiter?: string;
-}
-
-export interface DataImportInput extends BaseImportInput {
-    Objects: any[];
 }
