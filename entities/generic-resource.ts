21a3ab412aed62dd8c18ee3b34e6ffb818e823ee
roiaharon-pepperi <roi.a@pepperi.com>
2022-07-05 15:53:54 +0300

    add optional to SearchBody params

--- entities/generic-resource.ts
+++ entities/generic-resource.ts
@@ -1,10 +1,10 @@
 export interface SearchBody {
-    Where: string;
-    Page: number;
-    PageSize: number;
-    KeyList: string[];
-    UniqueFieldsList: string[];
-    UniqueFieldID: string;
-    IncludeCount: number;
-    Fields: string[];
+    Where?: string;
+    Page?: number;
+    PageSize?: number;
+    KeyList?: string[];
+    UniqueFieldsList?: string[];
+    UniqueFieldID?: string;
+    IncludeCount?: boolean;
+    Fields?: string[];
 }
