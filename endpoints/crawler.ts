962c40ebab0fdaaa9bfa0ba7f9e9700fc217fdcf
AviadG <aviad.g@pepperi.com>
2024-01-09 15:25:06 +0200

    output fix

--- endpoints/crawler.ts
+++ endpoints/crawler.ts
@@ -1,12 +1,12 @@
 import Endpoint from '../endpoint';
-import { CrawlerInput, CrawlerOutput } from '../entities';
+import { CodeJob, CrawlerInput, CrawlerOutput } from '../entities';
 import { PapiClient } from '../papi-client';
 
 export class CrawlerEndpoint extends Endpoint<CrawlerOutput> {
     constructor(service: PapiClient, protected url: string) {
         super(service, url);
     }
-    async crawl(input: CrawlerInput): Promise<CrawlerOutput> {
+    async crawl(input: CrawlerInput): Promise<CodeJob> {
         return await this.service.post(`${this.url}/crawl`, input);
     }
 }
